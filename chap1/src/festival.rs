e411ddda294ff3ca5a20f53dbc60c147b00703cb
Eunchong Yu <kroisse@gmail.com>
2016-02-21 13:03:56 +0900

    FESTIVAL: split old codes

--- chap1/src/festival.rs
+++ chap1/src/festival.rs
@@ -11,6 +11,8 @@
     - 복붙하기 껄끄러우니 테스트 코드를 별도 파일로 분리함.
     - 이제 시간을 줄여 보자. 앞서 푼 사람들의 기록을 보면 10ms 이내에 달성 가능함.
  */
+mod festival_test;
+
 use std::io;
 use std::cmp;
 
@@ -72,74 +74,6 @@ fn calculate(costs: &[u32], n_team: u32) -> f64 {
         }
     }
     f64::from(min_avg_cost)
-}
-
-/*
-    비용 배열   1  2  3  1  2  3
-    비용 부분합 1  3  6  7  9 12
-
-    범위 합 연산을 부분합 인덱스 두 번 + 뺄셈 한 번으로 해결할 수 있다.
-
-    하지만 133ms -> 75ms
-    - Vec::with_capacity()의 유무가 이렇게 큰 차이를 만듭니다.
- */
-fn calculate_v2(prices: &[u32], n_team: u32) -> f64 {
-    let n_team = n_team as usize;
-    assert!(n_team <= prices.len());
-    let mut min_avg_price = std::f64::MAX;
-    let mut partial_sum = Vec::with_capacity(prices.len() + 1);
-    partial_sum.push(0);
-    partial_sum.extend(prices.iter().scan(0, |state, &x| { *state += x; Some(*state) }));
-    let partial_sum = partial_sum;
-    for begin in 0..(prices.len() - n_team + 1) {
-        for end in (begin + n_team)..(prices.len() + 1) {
-            let sum = partial_sum[end] - partial_sum[begin];
-            let avg_price = sum as f64 / (end - begin) as f64;
-            if min_avg_price > avg_price {
-                min_avg_price = avg_price;
-            }
-        }
-    }
-    min_avg_price
-}
-
-// O(n^2), 83ms
-fn calculate_v1(prices: &[u32], n_team: u32) -> f64 {
-    let n_team = n_team as usize;
-    assert!(n_team <= prices.len());
-    let mut min_avg_price = std::f64::MAX;
-    for i in 0..(prices.len() - n_team + 1) {
-        let partial = &prices[i..prices.len()];
-        let scaned = partial.iter().zip(1..).scan(0, |state, (&x, n)| {
-            *state += x;
-            Some(if n < n_team { None } else { Some(*state as f64 / n as f64) })
-        }).skip_while(Option::is_none).map(Option::unwrap);
-        let avg_price = scaned.fold(std::f64::MAX, |acc, x| if acc > x { x } else { acc });
-        if min_avg_price > avg_price {
-            min_avg_price = avg_price
-        }
-    }
-    min_avg_price
-}
-
-// O(n^3), 약 700ms
-// 여기서 적어도 매 begin-end 쌍마다 range 안에서 또 루프를 도는 건 피할 수 있을 것 같다.
-fn calculate_v0(prices: &[u32], n_team: u32) -> f64 {
-    if n_team == 0 { return 0. }
-    let n_team = n_team as usize;
-    assert!(n_team <= prices.len());
-    let mut min_avg_price = std::f64::MAX;
-    for begin in 0..prices.len() {
-        for end in (begin + 1)..(prices.len() + 1) {
-            if end - begin < n_team { continue; }
-            let schedule = &prices[begin..end];
-            let avg_price = schedule.iter().fold(0, |acc, &x| acc + x) as f64 / schedule.len() as f64;
-            if min_avg_price > avg_price {
-                min_avg_price = avg_price;
-            }
-        }
-    }
-    min_avg_price
 }
 
 /*
@@ -222,31 +156,102 @@ fn process(input: &mut io::BufRead, output: &mut io::Write) {
     }
 }
 
-fn process_v1(input: &mut io::BufRead, output: &mut io::Write) {
-    let mut line = String::new();
-    input.read_line(&mut line).unwrap();
-    let cases: u32 = line.trim().parse().unwrap();
-    for _ in 0..cases {
-        line.clear();
-        input.read_line(&mut line).unwrap();
-        let mut info = Vec::with_capacity(2);
-        info.extend(line.trim().split(' ').map(|e| e.parse::<u32>().unwrap()));
-        let (days, teams) = (info[0] as usize, info[1]);
-        line.clear();
-        input.read_line(&mut line).unwrap();
-        let mut prices = Vec::with_capacity(days);
-        prices.extend(line.trim().split(' ').map(|e| e.parse::<u32>().unwrap()));
-        assert!(days as usize == prices.len());
-        let result = calculate(&prices, teams);
-        writeln!(output, "{:.11}", result).unwrap();
-    }
-}
-
 
 fn main() {
     let stdin = io::stdin();
     process(&mut stdin.lock(), &mut io::stdout());
 }
 
+mod old {
+    use std;
+    use std::io;
+    use std::cmp;
 
-mod festival_test;
+    /*
+        비용 배열   1  2  3  1  2  3
+        비용 부분합 1  3  6  7  9 12
+
+        범위 합 연산을 부분합 인덱스 두 번 + 뺄셈 한 번으로 해결할 수 있다.
+
+        하지만 133ms -> 75ms
+        - Vec::with_capacity()의 유무가 이렇게 큰 차이를 만듭니다.
+     */
+    fn calculate_v2(prices: &[u32], n_team: u32) -> f64 {
+        let n_team = n_team as usize;
+        assert!(n_team <= prices.len());
+        let mut min_avg_price = std::f64::MAX;
+        let mut partial_sum = Vec::with_capacity(prices.len() + 1);
+        partial_sum.push(0);
+        partial_sum.extend(prices.iter().scan(0, |state, &x| { *state += x; Some(*state) }));
+        let partial_sum = partial_sum;
+        for begin in 0..(prices.len() - n_team + 1) {
+            for end in (begin + n_team)..(prices.len() + 1) {
+                let sum = partial_sum[end] - partial_sum[begin];
+                let avg_price = sum as f64 / (end - begin) as f64;
+                if min_avg_price > avg_price {
+                    min_avg_price = avg_price;
+                }
+            }
+        }
+        min_avg_price
+    }
+
+    // O(n^2), 83ms
+    fn calculate_v1(prices: &[u32], n_team: u32) -> f64 {
+        let n_team = n_team as usize;
+        assert!(n_team <= prices.len());
+        let mut min_avg_price = std::f64::MAX;
+        for i in 0..(prices.len() - n_team + 1) {
+            let partial = &prices[i..prices.len()];
+            let scaned = partial.iter().zip(1..).scan(0, |state, (&x, n)| {
+                *state += x;
+                Some(if n < n_team { None } else { Some(*state as f64 / n as f64) })
+            }).skip_while(Option::is_none).map(Option::unwrap);
+            let avg_price = scaned.fold(std::f64::MAX, |acc, x| if acc > x { x } else { acc });
+            if min_avg_price > avg_price {
+                min_avg_price = avg_price
+            }
+        }
+        min_avg_price
+    }
+
+    // O(n^3), 약 700ms
+    // 여기서 적어도 매 begin-end 쌍마다 range 안에서 또 루프를 도는 건 피할 수 있을 것 같다.
+    fn calculate_v0(prices: &[u32], n_team: u32) -> f64 {
+        if n_team == 0 { return 0. }
+        let n_team = n_team as usize;
+        assert!(n_team <= prices.len());
+        let mut min_avg_price = std::f64::MAX;
+        for begin in 0..prices.len() {
+            for end in (begin + 1)..(prices.len() + 1) {
+                if end - begin < n_team { continue; }
+                let schedule = &prices[begin..end];
+                let avg_price = schedule.iter().fold(0, |acc, &x| acc + x) as f64 / schedule.len() as f64;
+                if min_avg_price > avg_price {
+                    min_avg_price = avg_price;
+                }
+            }
+        }
+        min_avg_price
+    }
+
+    fn process_v1(input: &mut io::BufRead, output: &mut io::Write) {
+        let mut line = String::new();
+        input.read_line(&mut line).unwrap();
+        let cases: u32 = line.trim().parse().unwrap();
+        for _ in 0..cases {
+            line.clear();
+            input.read_line(&mut line).unwrap();
+            let mut info = Vec::with_capacity(2);
+            info.extend(line.trim().split(' ').map(|e| e.parse::<u32>().unwrap()));
+            let (days, teams) = (info[0] as usize, info[1]);
+            line.clear();
+            input.read_line(&mut line).unwrap();
+            let mut prices = Vec::with_capacity(days);
+            prices.extend(line.trim().split(' ').map(|e| e.parse::<u32>().unwrap()));
+            assert!(days as usize == prices.len());
+            let result = super::calculate(&prices, teams);
+            writeln!(output, "{:.11}", result).unwrap();
+        }
+    }
+}
